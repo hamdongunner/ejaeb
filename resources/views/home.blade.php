565c5473194821bca0ed5b62bf89e7c89f470ac0
hamdon <hamdongunner@gmail.com>
2017-08-19 09:16:49 +0300

    the first pin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,66 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 500px;
+            width: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100px;
 
-    <head>
-        <style>
-            /* Always set the map height explicitly to define the size of the div
-             * element that contains the map. */
-            #map {
-                height: 100%;
-            }
-            /* Optional: Makes the sample page fill the window. */
-            html, body {
-                height: 100%;
-                margin: 0;
-                padding: 0;
-            }
-        </style>
-    </head>
-    <body>
+            margin: 0;
+            padding: 0;
+        }
+    </style>
     <div id="map"></div>
+    {{--<!-- Scripts -->--}}
     <script>
         var map;
         function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 2,
-                center: {lat: -33.865427, lng: 151.196123},
+                zoom: 12,
+                center: new google.maps.LatLng(33.312805,44.361488),
                 mapTypeId: 'terrain'
             });
 
             // Create a <script> tag and set the USGS URL as the source.
             var script = document.createElement('script');
 
-            // This example uses a local copy of the GeoJSON stored at
-            // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
             script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
             document.getElementsByTagName('head')[0].appendChild(script);
+        }
 
-            map.data.setStyle(function(feature) {
-                var magnitude = feature.getProperty('mag');
-                return {
-                    icon: getCircle(magnitude)
-                };
+        // Loop through the results array and place a marker for each
+        // set of coordinates.
+        window.eqfeed_callback = function(results) {
+            console.log(results);
+//            for (var i = 0; i < results.features.length; i++) {
+            var latLng = new google.maps.LatLng(33.312805,44.361488);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
             });
-        }
-
-        function getCircle(magnitude) {
-            return {
-                path: google.maps.SymbolPath.CIRCLE,
-                fillColor: 'red',
-                fillOpacity: .2,
-                scale: Math.pow(2, magnitude) / 2,
-                strokeColor: 'white',
-                strokeWeight: .5
-            };
-        }
-
-        function eqfeed_callback(results) {
-            map.data.addGeoJson(results);
+//            }
         }
     </script>
     <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBe5wskKH6ChjF04K4VVghlpC6sAd4L5z4&callback=initMap">
     </script>
-    </body>
 @endsection
